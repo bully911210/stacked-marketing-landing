891918041f7b500d122d29f10d012c99d4f73d9e
badenhorst.franz <franz@sigsolutions.co.za>
2026-03-18 07:43:09 +0200

    fix: update webhook URL to new Make.com endpoint

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const WHATSAPP_NUMBER = "27621779799";
 export const WHATSAPP_LINK = `https://wa.me/${WHATSAPP_NUMBER}?text=Hi%20Stacked%20Marketing%2C%20I%27m%20interested%20in%20the%20R1999%20website%20offer`;
 export const WHATSAPP_POST_SUBMIT_LINK = `https://wa.me/${WHATSAPP_NUMBER}?text=Hi%20Stacked%20Marketing%2C%20I%20just%20submitted%20the%20website%20form!`;
-export const WEBHOOK_URL = "https://hook.eu2.make.com/u9zvt7i22tl7bmh04hudka6a1622hoox";
+export const WEBHOOK_URL = "https://hook.eu2.make.com/rw71138jxaqdksi2i1ganhueiczxnx6g";
